55aff052ba42342dcdb901f659a493625aad1b88
narg_95_cp <SND\narg_95_cp>
2012-12-02 01:15:27 +0000

    Bmda started

--- source/Goldenberry/optimization/edas/BaseEda.py
+++ source/Goldenberry/optimization/edas/BaseEda.py
@@ -1,12 +1,16 @@
+import abc
 from Goldenberry.optimization.base.GbBaseOptimizer import GbBaseOptimizer
 
 class BaseEda(GbBaseOptimizer):
     """This class represents a solution with a cost associated."""
+    __metaclass__ = abc.ABCMeta
 
+    @abc.abstractmethod
     def result_distribution(self):
         """Provides the final estimated distribution."""
         pass
 
+    @abc.abstractmethod
     def ready(self):
         """Informs if the algorithm is ready to execute."""
         pass
