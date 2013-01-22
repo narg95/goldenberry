9bbe8964916503f3fe0c9777501a49e5ace7ac13
narg_95_cp <SND\narg_95_cp>
2013-01-22 00:48:58 +0000

    testing bmda

--- source/Goldenberry/test/optimization/edas/test_edas.py
+++ source/Goldenberry/test/optimization/edas/test_edas.py
@@ -1,13 +1,22 @@
-from Goldenberry.optimization.edas.Cga import Cga
+from Goldenberry.optimization.edas import Cga, Bmda
 from Goldenberry.optimization.cost_functions.functions import *
 from unittest import *
 
 class CgaTest(TestCase):
     """Test class for the Cga algorithm"""
     def test_basic(self):
-        cga = Cga()
+        cga = Cga.Cga()
         cga.setup(onemax(), 10, 20)
         result = cga.search()
+        self.assertTrue(result.params.all())
+        self.assertEqual(result.cost, 10)
+
+class BmdaTest(TestCase):
+    """Test class for the Bmda algorithm"""
+    def test_basic(self):
+        bmda = Bmda.Bmda()
+        bmda.setup(onemax(), 10, 20)
+        result = bmda.search()
         self.assertTrue(result.params.all())
         self.assertEqual(result.cost, 10)
 
