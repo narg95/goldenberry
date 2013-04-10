eba50e3f45b21de1f52d42fd227a3f8abd38a614
narg_95_cp <SND\narg_95_cp>
2013-04-10 11:18:19 +0000

    Pbilc, Umdac

--- source/Goldenberry/test/optimization/edas/test_edas.py
+++ source/Goldenberry/test/optimization/edas/test_edas.py
@@ -200,7 +200,54 @@ class TildaTest(TestCase):
             Tilda._estimate_gaussian(means, vars, acc_means, acc_vars, best, cand_size, learning_rate)
         self.assertAlmostEqual(new_means[0], 0.98, 2)
         self.assertAlmostEqual(new_vars[0], 4.026, 2)
-    
+
+class PbilTest(TestCase):
+    """Test class for the tilda algorithm"""
+    def test_basic(self):
+        tilda = Tilda()
+        tilda.setup(10, 30, learning_rate = 0.5)
+        tilda.cost_func = GbCostFunction(OneMax)
+        result = tilda.search()
+        #self.assertTrue((result.params < 0.1).all())
+        #self.assertGreaterEqual(result.cost, 8)
+
+    """Test if the reset function allows a new algorithm execution."""    
+    def test_reset(self):
+        tilda = Tilda()
+        tilda.setup(10, 20)
+        tilda.cost_func = GbCostFunction(OneMax)
+        result = tilda.search()
+        tilda.reset()
+        self.assertEqual(tilda.iters, 0)
+        self.assertNotEqual(tilda.distr, None)        
+        self.assertEqual(tilda.cost_func.evals, 0)
+   
+    def test_calculate_means_vars(self):
+        means = np.array([2.0])
+        vars = np.array([9.0])
+        acc_means = np.array([1.5])
+        acc_vars = np.array([4.0])
+        cand_size = 5
+        learning_rate = 0.6
+        best = GbSolution(np.array([3.0]), float("-Inf"))
+        new_means, new_vars = \
+            Tilda._estimate_gaussian(means, vars, acc_means, acc_vars, best, cand_size, learning_rate)
+        self.assertAlmostEqual(new_means[0], 1.79, 2)
+        self.assertAlmostEqual(new_vars[0], 4.026, 2)
+
+    def test_calculate_means_vars_no_best(self):
+        means = np.array([2.0])
+        vars = np.array([9.0])
+        acc_means = np.array([1.5])
+        acc_vars = np.array([4.0])
+        cand_size = 5
+        learning_rate = 0.6
+        best = GbSolution(None, float("-Inf"))
+        new_means, new_vars = \
+            Tilda._estimate_gaussian(means, vars, acc_means, acc_vars, best, cand_size, learning_rate)
+        self.assertAlmostEqual(new_means[0], 0.98, 2)
+        self.assertAlmostEqual(new_vars[0], 4.026, 2)
+
 class OptmizerTesterTest(TestCase):
 
     def test_basic(self):
