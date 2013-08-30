619c58c7de17e924b872f4753841c2e88177c8bc
narg_95_cp <SND\narg_95_cp>
2013-08-30 00:54:43 +0000

    Multiclass perceptron full unit tests

--- source/Goldenberry/test/classification/test_perceptron.py
+++ source/Goldenberry/test/classification/test_perceptron.py
@@ -11,8 +11,9 @@ class PerceptronTest(TestCase):
     """Perceptron tests."""
 
     def setUp(self):
-        self.training_set = Orange.data.Table(os.path.dirname(__file__) + "\\test_data_2d.tab")
-        self.X, self.Y, _ = self.training_set.to_numpy()
+        self.data_2d = Orange.data.Table(os.path.dirname(__file__) + "\\test_data_2d.tab")
+        self.data_3d = Orange.data.Table(os.path.dirname(__file__) + "\\test_data_3d.tab")
+        self.X, self.Y, _ = self.data_2d.to_numpy()
         self.Y = self.Y * 2 -1
     
     def test_basic(self):
@@ -23,26 +24,55 @@ class PerceptronTest(TestCase):
         self.assertTrue((self.Y == classes).all())
         self.assertTrue((scores != 0.0 ).all())
 
+    def test_basic_neural(self):
+        #data = Orange.data.Table("iris")
+        data = self.data_2d
+        learner = Orange.classification.neural.NeuralNetworkLearner()
+        classifier = learner(data)
+        for example in data:
+            classifier(example)
+
     def test_perceptor_learner_base(self):
         max_iter = 5
         learner = PerceptronLearner(max_iter = max_iter)
-        classifier = learner(self.training_set)
-        self.assertGreaterEqual(max_iter, learner.iters)
-        self.assertIsNotNone(classifier.W)
-        self.assertIsNotNone(classifier.B)
-        self.assertIsNotNone(classifier.predict)
+        classifier = learner(self.data_2d)
+        self.assertGreaterEqual(max_iter, learner.max_iter)
+        self.assertIsNotNone(classifier.learner)
         self.assertIsNotNone(classifier.domain)
 
-    def test_perceptor_learner_classifier(self):
-        max_iter = 5
+    def test_perceptor_learner_2d(self):
+        max_iter = 10
         learner = PerceptronLearner(max_iter = max_iter)
-        classifier = learner(self.training_set)
-        for item in self.training_set:
-            self.assertEqual(classifier(item), item.getclass())
+        classifier = learner(self.data_2d)
+        self.assertEquals(classifier.learner.__class__, Perceptron)
+
+    def test_perceptor_learner_4d(self):
+        max_iter = 10
+        learner = PerceptronLearner(max_iter = max_iter)
+        classifier = learner(self.data_3d)
+        self.assertEquals(classifier.learner.__class__, OneVsAllMulticlassLearner)
+
+    def test_perceptor_learner_classifier_2d(self):
+        max_iter = 10
+        learner = PerceptronLearner(max_iter = max_iter)
+        classifier = learner(self.data_2d)
+        for item in self.data_2d:
+            value, prob = classifier(item, Orange.core.GetBoth)
+            self.assertEqual(value, item.getclass())
+            self.assertEquals(len(prob), 2)            
+
+    def test_perceptor_learner_classifier_4d(self):
+        max_iter = 20
+        learner = PerceptronLearner(max_iter = max_iter)
+        classifier = learner(self.data_3d)
+        for item in self.data_3d:
+            value, prob = classifier(item, Orange.core.GetBoth)
+            self.assertEqual(value, item.getclass())
+            self.assertEquals(len(prob), 4)
 
     def test_integration_perceptron_orange_learner(self):
         data = Orange.data.Table(os.path.dirname(__file__) + "\\test_data_2d.tab")
-        max_iter = 5
+        max_iter = 10
         perceptron = PerceptronLearner(max_iter = max_iter)
         svm = Orange.classification.svm.SVMLearner()
         bayes = Orange.classification.bayes.NaiveLearner()
