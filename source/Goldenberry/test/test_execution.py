14bfc45bdbf9ee5d5c20962b23d361d3eda11bba
narg_95_cp <SND\narg_95_cp>
2013-09-07 17:11:36 +0000

    all unit test running

--- source/Goldenberry/test/test_execution.py
+++ source/Goldenberry/test/test_execution.py
@@ -10,22 +10,22 @@ from msvcrt import getch
 if __name__ == "__main__":
 
     test_modules = [
-                    'feature_selection.test.WKieraTest' #,
-                    #'optimization.cost_functions.CostFunctionTest' ,
-                    #'optimization.edas.test_edas.CgaTest',
-                    #'optimization.edas.test_edas.PbilTest',
-                    #'optimization.edas.test_edas.BmdaTest',
-                    #'optimization.edas.test_edas.TildaTest',
-                    #'optimization.edas.test_edas.PbilcTest',
-                    #'optimization.edas.test_edas.OptmizerTesterTest',
-                    #'statistics.test_distributions.BinomialTest',
-                    #'statistics.test_distributions.BivariateBinomialTest',
-                    #'statistics.test_distributions.BinomialContingencyTableTest',
-                    #'statistics.test_distributions.GaussianTest',
-                    #'statistics.test_distributions.GaussianTruncTest',
-                    #'classification.test_perceptron.PerceptronTest',
-                    #'classification.test_multiclass_learner.MulticlassLearnerTest',
-                    #'classification.test_kernels.KernelsTest'
+                    'feature_selection.test.WKieraTest' ,
+                    'optimization.cost_functions.CostFunctionTest' ,
+                    'optimization.edas.test_edas.CgaTest',
+                    'optimization.edas.test_edas.PbilTest',
+                    'optimization.edas.test_edas.BmdaTest',
+                    'optimization.edas.test_edas.TildaTest',
+                    'optimization.edas.test_edas.PbilcTest',
+                    'optimization.edas.test_edas.OptmizerTesterTest',
+                    'statistics.test_distributions.BinomialTest',
+                    'statistics.test_distributions.BivariateBinomialTest',
+                    'statistics.test_distributions.BinomialContingencyTableTest',
+                    'statistics.test_distributions.GaussianTest',
+                    'statistics.test_distributions.GaussianTruncTest',
+                    'classification.test_perceptron.PerceptronTest',
+                    'classification.test_multiclass_learner.MulticlassLearnerTest',
+                    'classification.test_kernels.KernelsTest'
                     ]    
     suite = unittest.TestLoader().loadTestsFromNames(test_modules)    
     runner = unittest.TextTestRunner(verbosity = 2)
