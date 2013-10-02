Dependency methods in BMDA and bug solved in SIM calculation

source/Goldenberry/optimization/edas/Bivariate.py
@@ -5,12 +5,19 @@ from Goldenberry.optimization.edas.GbBaseEda import GbBaseEda
 from Goldenberry.optimization.base.GbSolution import GbSolution
 import math
 
+class DependencyMethod:
+    chi2_test = 0,
+    sim = 1
+
 class Bmda(GbBaseEda):
     """Bivariate marginal distribution algorithm."""
 
+    def setup(self, var_size = 10, cand_size = 20, max_evals = 100, selection_rate = 50, learning_rate = 1, callback_func = None, dependency_method = DependencyMethod.chi2_test, **kwargs):
+        return super(Bmda, self).setup(var_size, cand_size, max_evals, selection_rate, learning_rate, callback_func, dependency_method = DependencyMethod.chi2_test, **kwargs)
+    
     def initialize(self):
         self.distr = BivariateBinomial( n = self.var_size)
-
+        
     def sample(self, sample_size, top_ranked, best):
         if top_ranked is None:
             return GbBaseEda.sample(self, sample_size, top_ranked, best)
@@ -21,16 +28,16 @@ class Bmda(GbBaseEda):
     def estimate(self, top_ranked, best):
         marginals = np.average(top_ranked, axis = 0)
         entropy =  - marginals * np.log2(marginals) - (1 - marginals) * np.log2(1 - marginals)
-        roots, children, cond_props = Bmda.build_graph(top_ranked, entropy)        
+        roots, children, cond_props = Bmda.build_graph(top_ranked, entropy, self.dependency_method)        
         self.distr = BivariateBinomial(p = marginals, cond_props = cond_props, children = children, roots = roots)
 
     @staticmethod
-    def build_graph(candidates, sort):
+    def build_graph(candidates, sort, method):
         
         var_size = candidates.shape[1]
         
-        # calculate chi_matrix
-        chi_matrix = Bmda._get_chisqr_matrix(candidates)
+        # calculate dependency matrix
+        dependency_matrix = Bmda.build_chi2_dependency_matrix(candidates) if method == DependencyMethod.chi2_test else Bmda.build_sim_dependency_matrix(candidates)
         
         #Check if there are vertices.
         if var_size < 1:
@@ -48,11 +55,11 @@ class Bmda(GbBaseEda):
         roots.append(v)
         cond_props[v] = []
         del A[idx]
-        A_.append(v)        
+        A_.append(v)
 
         while len(A) > 0:
-            v1, v2, chi = Bmda._max_chisqr(A, A_, chi_matrix)
-            if chi > 3.84:
+            v1, v2, chi = Bmda.get_max_dependency(A, A_, dependency_matrix)
+            if chi > 0.0:
                 children[v2].append(v1)
                 ctable = BinomialContingencyTable(candidates[:,[v2]], candidates[:, [v1]])
                 cond_props[v1] = ctable.PyGx[:, 1]                
@@ -70,20 +77,30 @@ class Bmda(GbBaseEda):
         return roots, children, cond_props
 
     @staticmethod
-    def _get_chisqr_matrix(candidates):
+    def build_chi2_dependency_matrix(candidates):
         _, length = candidates.shape
         chi_matrix = np.empty((length, length))
         for x,y in itr.combinations(xrange(length), 2):
             ctable = BinomialContingencyTable(candidates[:,[x]], candidates[:, [y]])
             chisquare = ctable.chisquare()
-            chi_matrix[x,y] = chi_matrix[y,x] = chisquare            
+            chi_matrix[x,y] = chi_matrix[y,x] = chisquare if chisquare >= 3.84 else 0.0
         return chi_matrix
 
     @staticmethod
-    def _max_chisqr(X, Y, chi_matrix):
+    def build_sim_dependency_matrix(candidates):
+        _, length = candidates.shape
+        sim_matrix = np.empty((length, length))
+        for x,y in itr.combinations(xrange(length), 2):
+            ctable = BinomialContingencyTable(candidates[:,[x]], candidates[:, [y]])
+            sim =  ctable.sim()
+            sim_matrix[x,y] = sim_matrix[y,x] = sim if sim > 1e-5 else 0.0
+        return sim_matrix
+
+    @staticmethod
+    def get_max_dependency(X, Y, dependency_matrix):
         indx = np.array([(x,y) for x,y in itr.product(X, Y)])
         rows, cols = indx[:, 0], indx[:,1]
-        maxidx = np.argmax(chi_matrix[rows, cols])
+        maxidx = np.argmax(dependency_matrix[rows, cols])
         (x,y) = indx[maxidx]
-        max_chi = chi_matrix[x,y] 
+        max_chi = dependency_matrix[x,y] 
         return x, y, max_chi

source/Goldenberry/tests/optimization/edas/test_edas.py
@@ -1,5 +1,5 @@
 from Goldenberry.optimization.edas.Univariate import Cga, Pbil, Tilda, Pbilc
-from Goldenberry.optimization.edas.Bivariate import Bmda
+from Goldenberry.optimization.edas.Bivariate import Bmda, DependencyMethod
 from Goldenberry.optimization.base.GbSolution import GbSolution
 from Goldenberry.optimization.cost_functions import *
 from Goldenberry.optimization.edas.GbBlackBoxTester import GbBlackBoxTester
@@ -72,7 +72,7 @@ class BmdaTest(TestCase):
     """Test the generation of the chi square matrix"""
     def test_shape_calculate_chisquare_matrix(self):
         pop = np.array(([[0, 0, 0, 1], [1, 0, 1, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 1, 1],  [1,1,0,0]]))
-        chi_matrix = Bmda._get_chisqr_matrix(pop)
+        chi_matrix = Bmda.build_chi2_dependency_matrix(pop)
         self.assertEqual(chi_matrix.shape, (4,4))
         expected = np.array([[0, 0, 0, 0],[0, 0, 0, 0], [0, 0, 0 , 0],[0, 0, 0, 0]])
         #Test for a total independent chi matrix.
@@ -81,7 +81,7 @@ class BmdaTest(TestCase):
     """Test the max chi square algorithm"""
     def test_max_chisquare_base(self):
         chi_matrix = np.array([[0.0,4.0,0.0],[4,0.0,5.0],[0.0, 5.0, 0.0]])
-        x,y,chi = Bmda._max_chisqr([0,1],[2], chi_matrix)
+        x,y,chi = Bmda.get_max_dependency([0,1],[2], chi_matrix)
         self.assertEqual(x, 1)
         self.assertEqual(y, 2)
         self.assertEqual(chi, 5.0)
@@ -89,7 +89,7 @@ class BmdaTest(TestCase):
     """Test the max chi square algorithm"""
     def test_max_chisquare_base_1(self):
         chi_matrix = np.array([[0.0,4.0,0.0],[4,0.0,5.0],[0.0, 5.0, 0.0]])
-        x,y,chi = Bmda._max_chisqr([1],[0, 2], chi_matrix)
+        x,y,chi = Bmda.get_max_dependency([1],[0, 2], chi_matrix)
         self.assertEqual(x, 1)
         self.assertEqual(y, 2)
         self.assertEqual(chi, 5.0)
@@ -97,7 +97,7 @@ class BmdaTest(TestCase):
     """Test the max chi square algorithm"""
     def test_max_chisquare_base_2(self):
         chi_matrix = np.array([[0.0,4.0,0.0],[4,0.0,5.0],[0.0, 5.0, 0.0]])
-        x,y,chi = Bmda._max_chisqr([0],[1, 2], chi_matrix)
+        x,y,chi = Bmda.get_max_dependency([0],[1, 2], chi_matrix)
         self.assertEqual(x, 0)
         self.assertEqual(y, 1)
         self.assertEqual(chi, 4.0)
@@ -105,16 +105,25 @@ class BmdaTest(TestCase):
     def test_generate_graph_all_independent(self):
         """Test that the algorithm generates no graph, all variables independent"""
         pop = np.array(([[0, 0, 0, 1], [1, 0, 1, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 1, 1]]))
-        roots, _, _ = Bmda.build_graph(pop, np.zeros(4))
+        roots, _, _ = Bmda.build_graph(pop, np.zeros(4), DependencyMethod.chi2_test)
         roots.sort()
         self.assertTrue(np.equal(roots, [0,1,2,3]).all())
 
     def test_generate_graph_with_dependencies(self):
         """Test that the algorithm generates a graph with two root nodes"""
         pop = np.array(([[0, 0, 1, 0], [1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 0], [1, 1, 0, 0]]))
-        roots, _, _ = Bmda.build_graph(pop, np.zeros(4))
+        roots, _, _ = Bmda.build_graph(pop, np.zeros(4), DependencyMethod.chi2_test)
         self.assertEqual(len(roots), 2)
         self.assertTrue(np.equal(roots, 3).any())
+
+    def test_basic_search_onemax_sim_method(self):
+        """Test class for the Bmda algorithm"""
+        bmda = Bmda()
+        bmda.setup(10, 40, dependency_method = DependencyMethod.sim)
+        bmda.cost_func = GbCostFunction(OneMax)
+        result = bmda.search()
+        self.assertGreaterEqual(result.params.sum(), 8.0)
+        self.assertGreaterEqual(result.cost, 8.0)
 
     def test_basic_search_onemax(self):
         """Test class for the Bmda algorithm"""