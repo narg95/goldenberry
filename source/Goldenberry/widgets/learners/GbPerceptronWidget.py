809255689b0474f4cdb46e696b1410c83bad821e
narg_95_cp <SND\narg_95_cp>
2013-10-05 21:53:34 +0000

    renaming to lower case

---
+++ source/Goldenberry/widgets/learners/GbPerceptronWidget.py
@@ -0,0 +1,102 @@
+"""
+<name>Perceptron</name>
+<description>Canonical Perceptron Algorithm</description>
+<contact>Nestor Rodriguez</contact>
+<icon>icons/perceptron.svg</icon>
+<priority>50</priority>
+
+"""
+from Goldenberry.widgets import *
+
+class GbPerceptronWidget(OWWidget):
+    """Widget for the perceptron algorithm"""
+    
+    #attributes
+    settingsList = ['learning_rate', 'max_evals', 'name']
+    learning_rate = 1.0
+    max_evals = 10
+    learner = None
+    classifier = None
+    preprocessor = None
+    kernel = None
+    data = None
+    name = "Perceptron"
+
+    def __init__(self, parent=None, signalManager=None):
+        OWWidget.__init__(self, parent, signalManager, 'Perceptron')
+        
+        self.setup_interfaces()
+        self.setup_ui() 
+
+    def setup_interfaces(self):
+        self.inputs = [("Data", Orange.core.ExampleTable, self.set_data),
+                       ("Preprocess", PreprocessedLearner, self.set_preprocessor),
+                       ("Kernel Function", GbKernel, self.set_kernel)]
+        self.outputs = [("Learner", Orange.core.Learner),
+                        ("Classifier", Orange.core.Classifier)]
+
+    def setup_ui(self):
+        # Loads the UI from an .ui file.
+        self.name = self.captionTitle
+        load_widget_ui(self)
+        
+        # Subscribe to signals
+        QObject.connect(self.buttonBox,QtCore.SIGNAL("accepted()"), self.accepted)
+        QObject.connect(self.buttonBox,QtCore.SIGNAL("rejected()"), self.rejected)
+
+        #set new binding controls
+        nameEditor = OWGUI.lineEdit(self, self, "name", label="Name")
+        learningEditor = OWGUI.lineEdit(self, self, "learning_rate", label="Learning Rage", valueType = float, validator = QDoubleValidator(0.0,1.0, 4, self.controlArea))
+        maxiterEditor = OWGUI.lineEdit(self, self, "max_evals", label="Max. Evaluations", valueType = int, validator = QIntValidator(1,10000, self.controlArea))
+        
+        self.paramBox.setLayout(QFormLayout(self.paramBox))
+        self.paramBox.layout().addRow(nameEditor.box, nameEditor)
+        self.paramBox.layout().addRow(learningEditor.box, learningEditor)
+        self.paramBox.layout().addRow(maxiterEditor.box, maxiterEditor)
+        
+    def accepted(self):
+        self.accept()
+        self.apply_settings()
+
+    def rejected(self):
+        self.reject()
+
+    def set_data(self,data):
+        self.data = self.isDataWithClass(data, Orange.core.VarTypes.Discrete, checkMissing=True) and data or None
+        print "data has been set"
+        self.apply_settings()
+
+    def set_preprocessor(self, pp):
+        self.preprocessor = pp
+
+    def set_kernel(self, kernel):
+        self.kernel = kernel
+
+    def apply_settings(self):
+        self.classifier = None
+        self.learner = PerceptronLearner(self.kernel, self.max_evals, self.learning_rate)        
+        
+        if self.preprocessor:
+            self.learner = self.preprocessor.wrapLearner(self.learner)
+
+        if None is not self.data:
+            self.classifier = self.learner(self.data)
+        
+        self.send("Learner", self.learner)
+        self.send("Classifier", self.classifier)
+        print "Learner and classifier was sent"
+
+    def handleNewSignals(self):
+        self.apply_settings()
+
+if __name__=="__main__":
+    test_widget()
+
+def test_widget():
+    appl = QApplication(sys.argv)
+    ow = GbPerceptronWidget()
+    ow.show()
+    appl.exec_()    
+    data = Orange.data.Table(os.path.dirname(__file__) + "\\test_data_2d.tab")
+    ow.set_data(data)
+    ow.classifier(data[0])    
---
+++ source/Goldenberry/widgets/optimization/GbTildaWidget.py
@@ -0,0 +1,31 @@
+"""
+<name>TILDA</name>
+<description>Population-Based incremental algorithm.</description>
+<contact>Nestor Rodriguez</contact>
+<icon>icons/Tilda.svg</icon>
+<priority>40</priority>
+
+"""
+
+from Goldenberry.widgets.optimization.GbBaseEdaWidget import GbBaseEdaWidget
+from Goldenberry.widgets import Tilda, GbCostFunction, GbBaseOptimizer, QDoubleValidator, OWGUI, GbSolution
+
+class GbTildaWidget(GbBaseEdaWidget):
+    """Widget for Tilda algorithm."""
+    
+    learning_rate = 1.0
+
+    def __init__(self, parent=None, signalManager=None):
+        self.optimizer = Tilda()
+        self.settingsList.append("learning_rate")
+        GbBaseEdaWidget.__init__(self, parent, signalManager, 'TILDA')
+        self.inputs = [("Cost Function", GbCostFunction, self.set_cost_function)]
+        self.outputs = [("Optimizer", GbBaseOptimizer), ("Solution", GbSolution)]
+    
+    def setup_ui(self):
+        GbBaseEdaWidget.setup_ui(self)
+        learning_editor = OWGUI.lineEdit(self, self, "learning_rate", label="Learning Rate", valueType = float, validator = QDoubleValidator(0.0, 1.0, 4, self.controlArea))
+        self.paramBox.layout().addRow(learning_editor.box, learning_editor)
+
+    def setup_optimizer(self):
+        self.optimizer.setup(self.cand_size, max_evals = self.max_evals, learning_rate = self.learning_rate)   
