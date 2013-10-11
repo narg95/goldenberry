456dbfae0375155aa8c4fccb957593efe86d5281
narg_95_cp <SND\narg_95_cp>
2013-10-11 14:03:57 +0000

    BMDA with MI method.  Some other improvements.

--- source/Goldenberry/feature_selection/WKiera.py
+++ source/Goldenberry/feature_selection/WKiera.py
@@ -4,25 +4,32 @@ from Orange.evaluation.scoring import CA
 import numpy as np
 from copy import deepcopy
 import Orange
+import threading as th
+import Queue
 
 class WKieraCostFunction(GbCostFunction):
     """WKiera cost function."""
 
-    def __init__(self, data,  learner, solution_weight = 0.1):
+    def __init__(self, data,  learner, solution_weight = 0.1, folds = 10):
         self.reset_statistics()
         self.learner = learner
         self.data = data
+        self.folds = folds
         self.solution_weight = solution_weight
         self.var_size = len(data.domain.attributes)
 
     def execute(self, solutions):
-        #TODO Omptimize for parallel execution
-        results = np.empty(len(solutions))
-        for idx, weight in enumerate(solutions):
-            #transforms the data base on the weigths given by each solution.
-            weighted_data = self.data.to_numpy("ac")[0] * np.concatenate((weight, [1]))
-            results[idx] = CA(cross_validation([deepcopy(self.learner)], Orange.data.Table(self.data.domain, weighted_data), folds = 10))[0]
+        results = np.empty(len(solutions), dtype = float)
+        threads = [None] * len(solutions)
+        for idx, weight in enumerate(solutions):                    
+            thread = th.Thread(target = test_solution, args = [self.learner, weight, self.data, results, idx, self.folds])
+            thread.start()
+            threads[idx] = thread
         
+        #sync all threads
+        for thread in threads:
+            thread.join()
+
         self._update_statistics(results)
         return results*(1 - self.solution_weight) + (1 - np.average(solutions, axis = 1)) * self.solution_weight
 
@@ -30,5 +37,7 @@ class WKieraCostFunction(GbCostFunction):
         for result in results:
             super(WKieraCostFunction, self)._update_statistics(result)
 
-    def weight_data(self, data, solution):
-        new_data = data.to_numpy("ac") * np.concatenate((solution,[1]))
+def test_solution(learner, weight, data, results, idx, folds):
+    #transforms the data base on the weights given by each solution.    
+    weighted_data = data.to_numpy("ac")[0] * np.concatenate((weight, [1]))
+    results[idx] = CA(cross_validation([deepcopy(learner)], Orange.data.Table(data.domain, weighted_data), folds = folds))[0]
--- source/Goldenberry/optimization/edas/Bivariate.py
+++ source/Goldenberry/optimization/edas/Bivariate.py
@@ -7,14 +7,21 @@ import math
 
 class DependencyMethod:
     """ This is an enumeration for the supported dependency methods"""
-    chi2_test = 0,
-    sim = 1
+    chi2_test = 0
+    mi = 1
+    sim = 2
+
+    _values_ = [chi2_test, mi, sim]
+
+    def __getitem__(self, index):
+        return self._values_[index]
+    
 
 class Bmda(GbBaseEda):
     """Bivariate marginal distribution algorithm."""
 
     def setup(self, cand_size = 20, max_evals = 100, selection_rate = 50, learning_rate = 1, callback_func = None, dependency_method = DependencyMethod.chi2_test, **kwargs):
-        return super(Bmda, self).setup(cand_size, max_evals, selection_rate, learning_rate, callback_func, dependency_method = DependencyMethod.chi2_test, **kwargs)
+        return super(Bmda, self).setup(cand_size, max_evals, selection_rate, learning_rate, callback_func, dependency_method = dependency_method, **kwargs)
     
     def initialize(self):
         self.distr = BivariateBinomial( n = self.var_size)
@@ -36,12 +43,17 @@ class Bmda(GbBaseEda):
         self.distr = BivariateBinomial(p = marginals, cond_props = cond_props, children = children, roots = roots)
 
     @staticmethod
+    def calc_dependency_matrix(candidates, method):
+        if method == DependencyMethod.chi2_test:
+            return Bmda.chi2_dependency_matrix(candidates)
+        elif method == DependencyMethod.mi:
+            return Bmda.mi_dependency_matrix(candidates)
+        return Bmda.sim_dependency_matrix(candidates)
+
+    @staticmethod
     def build_graph(candidates, sort, method):
-        
         var_size = candidates.shape[1]
-        
-        # calculate dependency matrix
-        dependency_matrix = Bmda.build_chi2_dependency_matrix(candidates) if method == DependencyMethod.chi2_test else Bmda.build_sim_dependency_matrix(candidates)
+        dependency_matrix = Bmda.calc_dependency_matrix(candidates, method)
         
         #Check if there are vertices.
         if var_size < 1:
@@ -81,7 +93,7 @@ class Bmda(GbBaseEda):
         return roots, children, cond_props
 
     @staticmethod
-    def build_chi2_dependency_matrix(candidates):
+    def chi2_dependency_matrix(candidates):
         _, length = candidates.shape
         chi_matrix = np.empty((length, length))
         for x,y in itr.combinations(xrange(length), 2):
@@ -91,13 +103,23 @@ class Bmda(GbBaseEda):
         return chi_matrix
 
     @staticmethod
-    def build_sim_dependency_matrix(candidates):
+    def mi_dependency_matrix(candidates):
+        _, length = candidates.shape
+        mi_matrix = np.empty((length, length))
+        for x,y in itr.combinations(xrange(length), 2):
+            ctable = BinomialContingencyTable(candidates[:,[x]], candidates[:, [y]])
+            mi =  ctable.mutual_inf()
+            mi_matrix[x,y] = mi_matrix[y,x] = mi if mi > 1e-3 else 0.0
+        return mi_matrix
+
+    @staticmethod
+    def sim_dependency_matrix(candidates):
         _, length = candidates.shape
         sim_matrix = np.empty((length, length))
         for x,y in itr.combinations(xrange(length), 2):
             ctable = BinomialContingencyTable(candidates[:,[x]], candidates[:, [y]])
             sim =  ctable.sim()
-            sim_matrix[x,y] = sim_matrix[y,x] = sim if sim > 1e-5 else 0.0
+            sim_matrix[x,y] = sim_matrix[y,x] = sim if sim > 1e-3 else 0.0
         return sim_matrix
 
     @staticmethod
